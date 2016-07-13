2a80161d663b233006023b9e267fe64ab4c3eb1a
MysteryVaibhav <mysteryvaibhav@gmail.com>
2016-07-14 00:30:05 +0530

    Added function to do basic math operations like plus and minus.

--- app/src/main/java/easycal/com/easycal/MainActivity.java
+++ app/src/main/java/easycal/com/easycal/MainActivity.java
@@ -1,26 +1,30 @@
 package easycal.com.easycal;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.Locale;
-
-import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtSpeechInput;
+    private TextView resultText;
     private ImageButton btnSpeak;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    //TODO: Temp variables below to be removed later
+    private final String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                                    "0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    private final String[] operator = {"plus", "minus", "-", "+"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        resultText = (TextView) findViewById(R.id.result);
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
 
         // hide the action bar
@@ -75,13 +80,67 @@ public class MainActivity extends AppCompatActivity {
 
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtSpeechInput.setText(result.get(0));
+                    String input = result.get(0);
+                    String output = computeValue(input);
+                    txtSpeechInput.setText(input);
+                    if(output!=null) {
+                        resultText.setText(output);
+                    } else {
+                        resultText.setText("Sorry, didn't get you !!!");
+                    }
                 }
                 break;
             }
 
         }
     }
+
+    /**
+     * TODO: @Kailash, imlpement this properly
+     * Takes a string as input and returns a mathematical expression as output in a string
+     * @param input
+     * @return
+     */
+    private String computeValue(String input) {
+        String result = null;
+        input = input.toLowerCase();
+        for(String op: operator) {
+            if(input.contains(op)) {
+                String[] parts = input.split(op);
+                String left = null;
+                String right = null;
+                int leftNum = 0;
+                int rightNum = 0;
+                if(parts.length==2) {
+                    int t=0;
+                    for (String num : nums) {
+                        if (parts[0].contains(num)) {
+                            left = num;
+                            leftNum = t%11;
+                            break;
+                        }
+                        t++;
+                    }
+                    t=0;
+                    for (String num : nums) {
+                        if (parts[1].contains(num)) {
+                            right = num;
+                            rightNum = t%11;
+                            break;
+                        }
+                        t++;
+                    }
+                    if(left!=null && right!=null) {
+                        String oprt = op.equals("-") || op.equals("minus")?"-":"+";
+                        int out = op.equals("-") || op.equals("minus")?leftNum-rightNum:leftNum+rightNum;
+                        result = leftNum + " " + oprt + " " + rightNum + " = " + out;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
