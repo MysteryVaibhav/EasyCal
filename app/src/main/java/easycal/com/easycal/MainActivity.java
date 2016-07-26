ec3050ac0420eaa48de0899684f6d90997329475
MysteryVaibhav <mysteryvaibhav@gmail.com>
2016-07-27 00:40:15 +0530

    added functionality to calculator buttons

--- app/src/main/java/easycal/com/easycal/MainActivity.java
+++ app/src/main/java/easycal/com/easycal/MainActivity.java
@@ -7,6 +7,7 @@ import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultText;
     private ImageButton btnSpeak;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    //Calculator
+    Button button0 , button1 , button2 , button3 , button4 , button5 , button6 ,
+            button7 , button8 , button9 , buttonAdd , buttonSub , buttonDivision ,
+            buttonMul , button10 , buttonC , buttonEqual ;
+    float mValueOne , mValueTwo ;
+    boolean mAddition , mSubtract ,mMultiplication ,mDivision ;
 
     //TODO: Temp variables below to be removed later
     private final String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
@@ -38,6 +46,24 @@ public class MainActivity extends AppCompatActivity {
         // hide the action bar
         //getActionBar().hide();
 
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
+        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmul);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -45,7 +71,153 @@ public class MainActivity extends AppCompatActivity {
                 promptSpeechInput();
             }
         });
+        //Calculator functions
 
+        //Number functions
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("0", true);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("1", true);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("2", true);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("3", true);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("4", true);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("5", true);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("6", true);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("7", true);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("8", true);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("9", true);
+            }
+        });
+
+        //Operators
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    mValueOne = Float.parseFloat(resultText.getText() + "");
+                    mAddition = true;
+                    resultText.setText(null);
+                    txtSpeechInput.setText(txtSpeechInput.getText()+" + ");
+                }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(resultText.getText() + "");
+                mSubtract = true ;
+                resultText.setText(null);
+                txtSpeechInput.setText(txtSpeechInput.getText()+" - ");
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(resultText.getText() + "");
+                mMultiplication = true ;
+                resultText.setText(null);
+                txtSpeechInput.setText(txtSpeechInput.getText()+" * ");
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(resultText.getText()+"");
+                mDivision = true ;
+                resultText.setText(null);
+                txtSpeechInput.setText(txtSpeechInput.getText()+" / ");
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(resultText.getText() + "");
+
+                if (mAddition == true){
+
+                    resultText.setText(mValueOne + mValueTwo +"");
+                    mAddition=false;
+                }
+
+                if (mSubtract == true){
+                    resultText.setText(mValueOne - mValueTwo+"");
+                    mSubtract=false;
+                }
+
+                if (mMultiplication == true){
+                    resultText.setText(mValueOne * mValueTwo+"");
+                    mMultiplication=false;
+                }
+
+                if (mDivision == true){
+                    resultText.setText(mValueOne / mValueTwo+"");
+                    mDivision=false;
+                }
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult("", false);
+            }
+        });
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTextInInputAndResult(".", true);
+            }
+        });
     }
 
     /**
@@ -147,6 +319,28 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    //Calculator functions
+    private void setTextInInputAndResult(String text, boolean append) {
+        setTextInInput(text, append);
+        setTextInResult(text, append);
+    }
+
+    private void setTextInInput(String text, boolean append) {
+        if(append) {
+            txtSpeechInput.setText(txtSpeechInput.getText() + text);
+        } else {
+            txtSpeechInput.setText(text);
+        }
+    }
+
+    private void setTextInResult(String text, boolean append) {
+        if(append) {
+            resultText.setText(resultText.getText() + text);
+        } else {
+            resultText.setText(text);
+        }
     }
 
 }
